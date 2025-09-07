38583f23b84186aab46587a67430a29be6e1eb7f
moomookcow <kwangmook.dev@gmail.com>
2025-09-07 13:46:37 +0900

    라우팅 기능 추가: App 컴포넌트에 React Router를 적용하여 여러 페이지(홈, 블로그, 소개, 방명록, 404 페이지)로의 내비게이션을 구현함.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "@/components/layout/Layout";
 import HomePage from "./pages/HomePage";
+import BlogListPage from "./pages/BlogListPage";
+import AboutPage from "./pages/AboutPage";
+import GuestbookPage from "./pages/GuestbookPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <Layout showSidebar={false}>
-      <HomePage />
-    </Layout>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout showSidebar={false}>
+              <HomePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <Layout showSidebar={true}>
+              <BlogListPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout showSidebar={false}>
+              <AboutPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/guestbook"
+          element={
+            <Layout showSidebar={false}>
+              <GuestbookPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Layout showSidebar={false}>
+              <NotFoundPage />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
