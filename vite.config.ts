a85b83ba5bfedcc35c0f204a402a9c9ab02d5ae6
moomookcow <kwangmook.dev@gmail.com>
2025-09-08 18:11:52 +0900

    vite.config.ts 파일에 MDX 플러그인 추가: @mdx-js/rollup을 사용하여 MDX 지원을 구현함.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    mdx({
+      providerImportSource: "@mdx-js/react",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
